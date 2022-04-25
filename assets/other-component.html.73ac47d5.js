d70751c9e06990c017e0cc0a946919384080cbce
Paulo <paulo@fazendadosoftware.com>
2022-04-25 16:56:13 -0300

    deploy

---
+++ assets/other-component.html.73ac47d5.js
@@ -0,0 +1,1 @@
+const e={key:"v-56484d99",path:"/components/other-component.html",title:"Other component",lang:"en",frontmatter:{sidebarDepth:0},excerpt:"",headers:[{level:2,title:"Props",slug:"props",children:[]},{level:2,title:"Events",slug:"events",children:[]},{level:2,title:"Slots",slug:"slots",children:[]}],git:{},filePathRelative:"components/other-component.md"};export{e as data};
